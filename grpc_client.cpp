cb26c022b5136cc35bd53f6a4b32430e1f9bb29d
mu-777 <water.tree.rainbow@gmail.com>
2021-07-28 02:51:35 +0900

    Mimimal grpc sample

---
+++ grpc_client.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <grpcpp/security/credentials.h>
+#include <grpcpp/create_channel.h>
+#include "simple.pb.h"
+#include "simple.grpc.pb.h"
+
+int main(int argc, char **argv) {
+  std::cout << "Hello, World! client" << std::endl;
+  if (argc < 2){
+    std::cout << "Must input 2 args" << std::endl;
+    return 0;
+  }
+  auto str1 = argv[1];
+  auto str2 = argv[2];
+
+  auto channel = grpc::CreateChannel("localhost:50051",
+                                     grpc::InsecureChannelCredentials());
+  auto stub = simple::Add::NewStub(channel);
+
+  simple::AddRequest req;
+  req.set_str1(str1);
+  req.set_str2(str2);
+
+  grpc::ClientContext ctx;
+  simple::AddReply res;
+  auto status = stub->Add(&ctx, req, &res);
+  if (!status.ok()) {
+    std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+    std::cout <<  "RPC failed" << std::endl;
+    return 0;
+  }
+  std::cout << "[Client] ReceivedRes: " << res.str() << std::endl;
+  return 0;
+}
+
---
+++ grpc_server.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+
+#include "simple.pb.h"
+#include "simple.grpc.pb.h"
+
+namespace simple {
+class SimpleAddImpl final : public Add::Service {
+  ::grpc::Status Add(::grpc::ServerContext *context, const AddRequest *req,
+                     AddReply *res) override {
+    std::cout << "[Server] ReceivedReq: " << req->str1() << ", " << req->str2() << std::endl;
+    res->set_str(req->str1() + req->str2());
+    return ::grpc::Status::OK;
+  }
+};
+};
+
+void RunServer() {
+  std::string server_address("localhost:50051");
+  simple::SimpleAddImpl service;
+
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  grpc::ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
+
+int main(int argc, char **argv) {
+  std::cout << "Hello, World! server" << std::endl;
+  RunServer();
+  return 0;
+}
+
