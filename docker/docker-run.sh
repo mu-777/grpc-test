03f2222357ec507f5b8ff7c85134bd55ffd5e24f
mu-777 <water.tree.rainbow@gmail.com>
2021-08-28 18:01:12 +0900

    auto remove container

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,2 +1,2 @@
-docker run -d --cap-add sys_ptrace -p127.0.0.1:2223:22 --name grpc_dev mu-777/grpc-cpp-dev
+docker run -d --rm --cap-add sys_ptrace -p127.0.0.1:2223:22 --name grpc_dev mu-777/grpc-cpp-dev
 ssh-keygen -f "$HOME/.ssh/known_hosts" -R localhost:2223
