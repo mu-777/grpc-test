a9c31ae74d10dd075f5e685dd4674858d3ea51ee
mu-777 <water.tree.rainbow@gmail.com>
2021-08-28 18:00:33 +0900

    Change container name

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,3 +1,3 @@
 cd $(dirname $0)
-docker build -t ryosuke/grpc-cpp -f ./Dockerfile.grpc .
-docker build -t ryosuke/grpc-cpp-dev -f ./Dockerfile.grpc-dev .
+docker build -t mu-777/grpc-cpp -f ./Dockerfile.grpc .
+docker build -t mu-777/grpc-cpp-dev -f ./Dockerfile.grpc-dev .
