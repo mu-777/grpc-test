c82b010c7a616dbb93b54e0f5157fc5e14f0a323
mu-777 <water.tree.rainbow@gmail.com>
2021-12-05 12:48:34 +0900

    Add reflection server code

--- src/reflection/reflection_server.h
+++ src/reflection/reflection_server.h
@@ -6,14 +6,35 @@
 
 namespace reflection {
 class ReflectionImpl final : public Area::Service {
+
+  const float PI = 3.14159265f;
+
   grpc::Status CalcArea(grpc::ServerContext *context,
                         const reflection::ShapeData *request,
                         reflection::AreaData *response) override {
     std::cout << "Received data" << std::endl;
-    response->set_area(1.0f);
+    auto s = CalcAreaImpl(request);
+    std::cout << "Calced: " << s << std::endl;
+    response->set_area(s);
     return ::grpc::Status::OK;
   }
 
+  float CalcAreaImpl(const reflection::ShapeData *sd) {
+    switch (sd->shape_case()) {
+      case ShapeData::ShapeCase::kTriangle:
+        return sd->triangle().baseside() * sd->triangle().height() * 0.5f;
+      case ShapeData::ShapeCase::kRectangle:
+        return sd->rectangle().side1() * sd->rectangle().side2();
+      case ShapeData::ShapeCase::kSquare:
+        return sd->square().side() * sd->square().side();
+      case ShapeData::ShapeCase::kCircle:
+        return sd->circle().radius() * sd->circle().radius() * PI;
+      case ShapeData::ShapeCase::SHAPE_NOT_SET:
+        std::cout << "SHAPE_NOT_SET" << std::endl;
+        return 0;
+    }
+    return 0;
+  }
 };
 };
 
