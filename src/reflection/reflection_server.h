5e7ae4ef5f83cbe95aefd5446a68723ce7b69cf2
mu-777 <water.tree.rainbow@gmail.com>
2021-12-11 15:34:16 +0900

    refactor reflection server

--- src/reflection/reflection_server.h
+++ src/reflection/reflection_server.h
@@ -4,37 +4,79 @@
 #include "reflection.pb.h"
 #include "reflection.grpc.pb.h"
 
+#include <functional>
+
 namespace reflection {
 class ReflectionImpl final : public Area::Service {
 
-  const float PI = 3.14159265f;
-
   grpc::Status CalcArea(grpc::ServerContext *context,
-                        const reflection::ShapeData *request,
-                        reflection::AreaData *response) override {
+                        const ShapeData *request,
+                        AreaData *response) override {
     std::cout << "Received data" << std::endl;
     auto s = CalcAreaImpl(request);
+    assert(s == CalcAreaImpl2(request));
     std::cout << "Calced: " << s << std::endl;
     response->set_area(s);
     return ::grpc::Status::OK;
   }
 
-  float CalcAreaImpl(const reflection::ShapeData *sd) {
+  float CalcAreaImpl(const ShapeData *sd) {
     switch (sd->shape_case()) {
       case ShapeData::ShapeCase::kTriangle:
-        return sd->triangle().baseside() * sd->triangle().height() * 0.5f;
+        return CalcTriangleArea(sd->triangle());
       case ShapeData::ShapeCase::kRectangle:
-        return sd->rectangle().side1() * sd->rectangle().side2();
+        return CalcRectangleArea(sd->rectangle());
       case ShapeData::ShapeCase::kSquare:
-        return sd->square().side() * sd->square().side();
+        return CalcSquareArea(sd->square());
       case ShapeData::ShapeCase::kCircle:
-        return sd->circle().radius() * sd->circle().radius() * PI;
+        return CalcCircleArea(sd->circle());
       case ShapeData::ShapeCase::SHAPE_NOT_SET:
         std::cout << "SHAPE_NOT_SET" << std::endl;
         return 0;
     }
     return 0;
   }
+
+  float CalcAreaImpl2(const ShapeData *sd) {
+    std::unordered_map<ShapeData::ShapeCase, std::function<float(const ShapeData *)>> switcher = {
+            {ShapeData::ShapeCase::kTriangle,
+                    [&](const ShapeData *sd) { return CalcTriangleArea(sd->triangle()); }},
+            {ShapeData::ShapeCase::kRectangle,
+                    [&](const ShapeData *sd) { return CalcRectangleArea(sd->rectangle()); }},
+            {ShapeData::ShapeCase::kSquare,
+                    [&](const ShapeData *sd) { return CalcSquareArea(sd->square()); }},
+            {ShapeData::ShapeCase::kCircle,
+                    [&](const ShapeData *sd) { return CalcCircleArea(sd->circle()); }},
+    };
+    return switcher.at(sd->shape_case())(sd);
+  }
+
+  template<class ProtoMsgType>
+  static ProtoMsgType parse(const ShapeData sd) {
+    ProtoMsgType ret;
+    auto oneof_param_name = "shape";
+    auto oneof_desc = sd.GetDescriptor()->FindOneofByName(oneof_param_name);
+    auto field_desc = sd.GetReflection()->GetOneofFieldDescriptor(sd, oneof_desc);
+    ret.ParseFromString(sd.GetReflection()->GetMessage(sd, field_desc).SerializeAsString());
+    return ret;
+  }
+
+  std::function<float(Triangle)> CalcTriangleArea = [](Triangle t) {
+    return t.baseside() * t.height() * 0.5f;
+  };
+
+  std::function<float(Rectangle)> CalcRectangleArea = [](Rectangle r) {
+    return r.side1() * r.side2();
+  };
+
+  std::function<float(Square)> CalcSquareArea = [](Square s) {
+    return s.side() * s.side();
+  };
+
+  std::function<float(Circle)> CalcCircleArea = [](Circle c) {
+    static const float PI = 3.14159265f;
+    return c.radius() * c.radius() * PI;
+  };
 };
 };
 
